bd8bf6bfc7bf0a5976df40003701f26c135a4c84
albamarroschmitt <albamarroschmitt@gmail.com>
2026-03-08 08:39:52 +0100

    worked on basic API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
+function updateWeather(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let temperatureElement = document.querySelector(
+    "#weather-current-temperature",
+  );
+  temperatureElement.innerHTML = temperature;
+}
+
+function searchCity(cityName) {
+  let apiKey = "9a9b8262d5b0t51o000cfc108a873741";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${encodeURIComponent(
+    cityName,
+  )}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
+
   let searchInputCity = document.querySelector("#city");
   let cityElement = document.querySelector("#weather-city");
+
   cityElement.innerHTML = searchInputCity.value;
+  searchCity(searchInputCity.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("Barcelona");
